3fc392a3b01ca1d708e754f40fb081342163670e
Melnikov Viktor <sukhvost@yandex.ru>
2020-06-13 16:34:34 +0300

    added structure of header

--- src/moduls/header/header.js
+++ src/moduls/header/header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Logo from "../../components/logo";
+import Link from "../../components/link";
 
 import './header.css';
 
 const Header = () => {
     return (
-        <header className={'header-app'}></header>
+        <header className={'header-app'}>
+            <Logo/>
+            <div className={'header-links'}>
+                <Link/>
+            </div>
+        </header>
     );
 }
 
